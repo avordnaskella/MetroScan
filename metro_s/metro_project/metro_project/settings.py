7af80d7f9b1953e2c2f3b14f94e09464baa3739b
Александрова Арина Алексеевна <aleksandrova.aa@metronn.ru>
2026-06-23 11:53:35 +0300

    разработка дизайна

--- metro_s/metro_project/metro_project/settings.py
+++ metro_s/metro_project/metro_project/settings.py
@@ -116,9 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+
 
 DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
